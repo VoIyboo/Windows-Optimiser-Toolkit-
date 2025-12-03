ddb1bdc03f996816bab9fbe539b640a376558bb7
VoIyboo <amillar566@outlook.com>
2025-12-03 15:52:12 +1100

    Update Advanced.psm1

--- src/Modules/Advanced.psm1
+++ src/Modules/Advanced.psm1
@@ -1,5 +1,7 @@
+. "$PSScriptRoot\..\Core\Logging.psm1"
+
 function Invoke-QAdvancedOptions {
-    Write-QLog "Advanced options triggered."
+    Write-QLog "Advanced options executed (stub)."
 }
 
 Export-ModuleMember -Function Invoke-QAdvancedOptions
