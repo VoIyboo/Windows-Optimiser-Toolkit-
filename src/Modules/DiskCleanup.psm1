bbac78196af49f6c0ba7943c41cd0cac3927e253
VoIyboo <amillar566@outlook.com>
2025-12-03 15:50:56 +1100

    Update DiskCleanup.psm1

--- src/Modules/DiskCleanup.psm1
+++ src/Modules/DiskCleanup.psm1
@@ -1,5 +1,7 @@
+. "$PSScriptRoot\..\Core\Logging.psm1"
+
 function Invoke-QDiskCleanup {
-    Write-QLog "Disk cleanup invoked."
+    Write-QLog "Disk cleanup invoked (stub)."
 }
 
 Export-ModuleMember -Function Invoke-QDiskCleanup
