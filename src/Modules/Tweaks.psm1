7bdc2842a8cb4e0ab394355af2e9f2411f629801
VoIyboo <amillar566@outlook.com>
2025-12-03 15:51:54 +1100

    Update Tweaks.psm1

--- src/Modules/Tweaks.psm1
+++ src/Modules/Tweaks.psm1
@@ -1,9 +1,11 @@
+. "$PSScriptRoot\..\Core\Logging.psm1"
+
 function Invoke-QSafeTweaks {
-    Write-QLog "Safe tweaks applied."
+    Write-QLog "Safe tweaks applied (stub)."
 }
 
 function Invoke-QDebloatTweaks {
-    Write-QLog "Debloat tweaks applied."
+    Write-QLog "Debloat tweaks applied (stub)."
 }
 
 Export-ModuleMember -Function Invoke-QSafeTweaks, Invoke-QDebloatTweaks
