d9448b6104dae5b9519092f074453201cd3e4d26
Studio Voly <amillar566@outlook.com>
2025-12-05 13:55:08 +1100

    Update QuinnOptimiserToolkit-v2.7.ps1

--- src/Legacy/QuinnOptimiserToolkit-v2.7.ps1
+++ src/Legacy/QuinnOptimiserToolkit-v2.7.ps1
@@ -48,6 +48,8 @@ Import-Module (Join-Path $srcRoot "Modules\QOT.Actions.Clean.psm1")    -Force
 Import-Module (Join-Path $srcRoot "Modules\QOT.Actions.Tweaks.psm1")   -Force
 Import-Module (Join-Path $srcRoot "Modules\QOT.Actions.Advanced.psm1") -Force
 Import-Module (Join-Path $srcRoot "Modules\QOT.Apps.psm1")             -Force
+Import-Module (Join-Path $srcRoot "Modules\QOT.UI.Apps.psm1")          -Force
+
 
 # ------------------------------
 # System summary
