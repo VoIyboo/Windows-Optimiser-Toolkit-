bf0a0d6c8c482c2d97eae9c7392f3a760843a971
Studio Voly <amillar566@outlook.com>
2025-12-05 13:11:31 +1100

    Update QOT.Apps.psm1

--- src/Modules/QOT.Apps.psm1
+++ src/Modules/QOT.Apps.psm1
@@ -1,1 +1,29 @@
+# ------------------------------
+# App scan and risk logic
+# ------------------------------
+$Global:AppWhitelistPatterns = @(
+    ...
+)
 
+function Get-InstalledApps { ... }
+
+function Get-AppRisk { ... }
+
+# ------------------------------
+# Install apps (winget) helpers
+# ------------------------------
+function Test-AppInstalledWinget { ... }
+
+function Install-AppWithWinget { ... }
+
+function Initialise-InstallAppsList { ... }
+
+function Install-SelectedCommonApps { ... }
+
+Export-ModuleMember -Variable AppWhitelistPatterns -Function `
+    Get-InstalledApps, `
+    Get-AppRisk, `
+    Test-AppInstalledWinget, `
+    Install-AppWithWinget, `
+    Initialise-InstallAppsList, `
+    Install-SelectedCommonApps
