28526e8d61e263f5395260805b0cc8e8937b666e
Studio Voly <amillar566@outlook.com>
2025-12-09 11:00:01 +1100

    Update Intro.ps1

--- src/Intro/Intro.ps1
+++ src/Intro/Intro.ps1
@@ -10,9 +10,10 @@ $ErrorActionPreference = "Stop"
 # ------------------------------
 # Load core modules
 # ------------------------------
-Import-Module "$PSScriptRoot\..\Core\Config.psm1"  -Force
-Import-Module "$PSScriptRoot\..\Core\Logging.psm1" -Force
-Import-Module "$PSScriptRoot\..\Core\Engine.psm1"  -Force
+Import-Module "src\Core\Config\Config.psm1"   -Force
+Import-Module "src\Core\Logging\Logging.psm1" -Force
+Import-Module "src\Core\Engine\Engine.psm1"   -Force
+
 # Later we will also import the splash UI here
 # Import-Module "$PSScriptRoot\Splash.UI.psm1" -Force
 
