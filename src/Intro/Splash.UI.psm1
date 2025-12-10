1f1fa1828d614f7b4de8cd069aff593fb9a986c3
Studio Voly <amillar566@outlook.com>
2025-12-10 11:07:06 +1100

    Update Splash.UI.psm1

--- src/Intro/Splash.UI.psm1
+++ src/Intro/Splash.UI.psm1
@@ -11,31 +11,31 @@ function New-QOTSplashWindow {
         throw "Splash XAML not found at: $Path"
     }
 
-    # Load XAML
     $xamlContent = Get-Content $Path -Raw
-    $window      = [Windows.Markup.XamlReader]::Parse($xamlContent)
+    $window = [Windows.Markup.XamlReader]::Parse($xamlContent)
 
-    # Try to wire up the Studio Voly logo image manually
+    # Try to wire up the Studio Voly logo from disk
     try {
         $logoControl = $window.FindName("LogoImage")
-        if ($logoControl -and $logoControl -is [System.Windows.Controls.Image]) {
-
-            # Image lives next to Splash.xaml
-            $xamlFolder = Split-Path $Path -Parent
-            $logoPath   = Join-Path $xamlFolder "StudioVolySplash.png"
+        if ($logoControl) {
+            # repo root = parent of parent of this module folder (src\Intro)
+            $rootPath = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
+            $logoPath = Join-Path $rootPath "src\Intro\StudioVolySplash.png"
 
             if (Test-Path $logoPath) {
                 $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
                 $bitmap.BeginInit()
-                $bitmap.UriSource   = New-Object System.Uri($logoPath, [System.UriKind]::Absolute)
+                $bitmap.UriSource = New-Object System.Uri($logoPath, [System.UriKind]::Absolute)
                 $bitmap.CacheOption = [System.Windows.Media.Imaging.BitmapCacheOption]::OnLoad
                 $bitmap.EndInit()
 
                 $logoControl.Source = $bitmap
+            } else {
+                Write-Host "Splash logo not found at $logoPath" -ForegroundColor Yellow
             }
         }
     } catch {
-        # If anything fails, we just skip the logo rather than crashing the splash
+        Write-Host "Failed to load splash logo: $($_.Exception.Message)" -ForegroundColor Yellow
     }
 
     return $window
@@ -67,7 +67,5 @@ function Update-QOTSplashProgress {
     } catch { }
 }
 
-Export-ModuleMember -Function `
-    New-QOTSplashWindow, `
-    Update-QOTSplashStatus, `
-    Update-QOTSplashProgress
+# Clean export list
+Export-ModuleMember -Function New-QOTSplashWindow, Update-QOTSplashStatus, Update-QOTSplashProgress
