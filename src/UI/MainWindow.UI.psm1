767eb8418c53b0c6322c00f2a5a403f1c54fdf54
Studio Voly <amillar566@outlook.com>
2025-12-12 08:09:58 +1100

    Update MainWindow.UI.psm1

--- src/UI/MainWindow.UI.psm1
+++ src/UI/MainWindow.UI.psm1
@@ -4,6 +4,7 @@
 Import-Module "$PSScriptRoot\..\Core\Config\Config.psm1"   -Force
 Import-Module "$PSScriptRoot\..\Core\Logging\Logging.psm1" -Force
 Import-Module "$PSScriptRoot\..\Core\Settings.psm1"        -Force  # NEW
+Import-Module "$PSScriptRoot\..\Core\Tickets.psm1"         -Force   # NEW
 Import-Module "$PSScriptRoot\..\Apps\Apps.UI.psm1"         -Force
 
 # Keep references to the window and key controls inside this module
