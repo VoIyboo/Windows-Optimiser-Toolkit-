7f0f9caa8414c07d6bafa30ad0af3c28503cb089
Studio Voly <amillar566@outlook.com>
2025-12-17 09:42:47 +1100

    Update Tickets.UI.psm1

--- src/Tickets/Tickets.UI.psm1
+++ src/Tickets/Tickets.UI.psm1
@@ -363,27 +363,31 @@ function Initialize-QOTicketsUI {
 $rowDetailsXaml = @"
 <DataTemplate xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
               xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
-  <Border Margin="10,4,10,10"
-          CornerRadius="6"
-          BorderThickness="1"
-          BorderBrush="#374151"
-          Background="#020617"
-          Padding="10"
-          HorizontalAlignment="Stretch">
-    <ScrollViewer VerticalScrollBarVisibility="Auto"
-              HorizontalScrollBarVisibility="Disabled"
-              PanningMode="VerticalFirst"
-              CanContentScroll="False"
-              MaxHeight="{Binding RelativeSource={RelativeSource AncestorType=DataGrid}, Path=Tag}"
-              HorizontalAlignment="Stretch">
-      <TextBlock Text="{Binding EmailBody}"
-             Foreground="White"
-             TextWrapping="Wrap"
-             FontSize="13"
-             LineHeight="18"
-             HorizontalAlignment="Stretch"/>
-    </ScrollViewer>
-  </Border>
+        <Border Margin="10,4,10,10"
+                CornerRadius="6"
+                BorderThickness="1"
+                BorderBrush="#374151"
+                Background="#020617"
+                Padding="10"
+                HorizontalAlignment="Stretch">
+        
+          <ScrollViewer VerticalScrollBarVisibility="Auto"
+                        HorizontalScrollBarVisibility="Disabled"
+                        PanningMode="VerticalFirst"
+                        CanContentScroll="False"
+                        MaxHeight="{Binding RelativeSource={RelativeSource AncestorType=DataGrid}, Path=Tag}"
+                        Width="{Binding RelativeSource={RelativeSource AncestorType=DataGridRow}, Path=ActualWidth}"
+                        HorizontalAlignment="Stretch">
+        
+            <TextBlock Text="{Binding EmailBody}"
+                       Foreground="White"
+                       TextWrapping="Wrap"
+                       FontSize="13"
+                       LineHeight="18"
+                       HorizontalAlignment="Stretch"/>
+        
+          </ScrollViewer>
+        </Border>
 </DataTemplate>
 "@
 
