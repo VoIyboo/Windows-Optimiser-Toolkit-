db3b62e616ba78d91578739d10be8596415bab4e
Studio Voly <amillar566@outlook.com>
2025-12-18 18:32:25 +1100

    Update Tickets.UI.psm1

--- src/Tickets/Tickets.UI.psm1
+++ src/Tickets/Tickets.UI.psm1
@@ -678,13 +678,9 @@ function Initialize-QOTicketsUI {
     })
 
     $BtnRefreshTickets.Add_Click({
-    try {
-        if (Get-Command Invoke-QOEmailTicketPoll -ErrorAction SilentlyContinue) {
-            Invoke-QOEmailTicketPoll | Out-Null
-        }
-    } catch { }
     Update-QOTicketsGrid
 })
+
 
 
     if ($BtnDeleteTicket) {
