7b84b948a88d17250ceb5f75caccbcf6be81bae6
Studio Voly <amillar566@outlook.com>
2026-02-05 11:46:07 +1100

    Create Invoke-QTweakAdvertisingId.ps1

---
+++ src/Scripts/Tweaks/Invoke-QTweakAdvertisingId.ps1
@@ -0,0 +1,9 @@
+param(
+    [object]$Window
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module "$PSScriptRoot\..\..\TweaksAndCleaning\TweaksAndPrivacy\TweaksAndPrivacy.psm1" -Force -ErrorAction Stop
+
+Invoke-QTweakAdvertisingId
